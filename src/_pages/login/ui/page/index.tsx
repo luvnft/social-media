3c483e7569cb62bb40f52d402dcff85f65f6f6cb
Shadiyarov Ansar <shadiyarovansar@gmail.com>
2024-05-12 12:46:45 +0500

    worked on auth

--- src/_pages/login/ui/page/index.tsx
+++ src/_pages/login/ui/page/index.tsx
@@ -1,7 +1,29 @@
 import { IProps } from "./props";
 
+import { login } from "@/processes";
+
 const Login = (props: IProps) => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <form className="flex flex-col">
+        <input
+          type="email"
+          name="email"
+          id="email"
+          className="border border-slate-400 outline-none"
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          className="border border-slate-400 outline-none"
+          required
+        />
+        <button formAction={login}>Log In</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
--- src/_pages/signup/ui/page/index.tsx
+++ src/_pages/signup/ui/page/index.tsx
@@ -1,8 +1,32 @@
 import React from "react";
 import { IProps } from "./props";
 
+import { signup } from "@/processes";
+
 const Signup = (props: IProps) => {
-  return <div>Signup</div>;
+  return (
+    <div className="h-screen flex flex-col justify-center items-center">
+      <form className="flex flex-col gap-y-4 w-[400px] mx-auto border border-slate-400 rounded-md p-6">
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Email"
+          className="border border-slate-400 outline-none py-2 px-4 rounded-sm"
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Password"
+          className="border border-slate-400 outline-none py-2 px-4"
+          required
+        />
+        <button formAction={signup}>Sign Up</button>
+      </form>
+    </div>
+  );
 };
 
 export default Signup;
